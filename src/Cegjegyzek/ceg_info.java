a0d208b1252aeb909c356f9b8c196909f1729e4d
Allen0246 <kelemen.rihcard1@gmail.com>
2022-04-27 11:26:44 +0200

    beadandó terv felvázolása

--- src/Cegjegyzek/ceg_info.java
+++ src/Cegjegyzek/ceg_info.java
@@ -3,6 +3,7 @@ package Cegjegyzek;
 //ha se alaptőke se beltag nincs megadva akkor ne fusson le a programm
 
 public class ceg_info {
+    int alakulas;
     int bekereules;
     int letszam;
     int alaptoke;
